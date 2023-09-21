86d688d4783a9d8c30022c45cfd142b8d6dfa57c
RobinB87 <robinbette@hotmail.com>
2023-09-21 10:07:36 +0000

    caching explained

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -4,7 +4,12 @@ interface User {
 }
 
 const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  // next js stores in a data cache based on a file system
+  // if the same url is fetched, next js gets it from the file system
+  // you have full control though, eg 'url', { cache: "no-store" }): always get new data
+  // or: 'url', { next: { revalidate: 10 } }) get new data every 10 seconds
+  // This is only implemented in the fetch function, NOT in for example axios!
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", { next: { revalidate: 100 } });
   const users: User[] = await res.json();
 
   return (
