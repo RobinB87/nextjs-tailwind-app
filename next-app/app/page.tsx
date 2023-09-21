c6b8cdcf2ea06377c31fe4d90637bbc97302b573
RobinB87 <robinbette@hotmail.com>
2023-09-21 11:00:01 +0000

    some commoents

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main>
       <h1>Hello world</h1>
+      {/* Next.js provides the Link component to enable client-side navigation. This means as the user navigates between pages, the new content is loaded quickly and smoothly without the entire page being reloaded.  */}
       <Link href="/users/">Users</Link>
       <ProductCard />
     </main>
--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -15,6 +15,11 @@ const UsersPage = async () => {
   return (
     <>
       <h1>Users:</h1>
+      {/* In Next.js, components can be rendered at build time (called Static Rendering) or at request time (called Dynamic Rendering). If we have pages or components with static data, we can pre-render them during build time to improve our application’s performance. 
+      
+      Rendering can happend static or dynamic. If no cache options are used in the fetch method, the below date will not change for a production build. 
+      When 'cache: "no-store"' is used, You will see a lambda in the build output, before the /users page, and an explanation that rendering happens at run time on the server side and not static */}
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((x) => (
           <li key={x.id}>{x.name}</li>
