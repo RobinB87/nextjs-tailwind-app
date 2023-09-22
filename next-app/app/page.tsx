f8cb35119b66654950bc42e8714a7526213c8b9b
RobinB87 <robinbette@hotmail.com>
2023-09-22 09:41:20 +0000

    comments about Link

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
+
 import ProductCard from "./components/ProductCard/ProductCard";
 
+// Link:
+//  - only downloads content of the target page (so if you click /users, it does not download the navbar etc). rsc in devtools stands for react_server_component
+//  - pre-fetches links that are in the viewport. If you run the app in production, the users page will be prefetched twice, once sorted by name and once sorted by email (as there are two Links). Improves performance
+//  - Caches pages on the client: if you go to a page you have seen before, nextjs does not do a request to backend. So if you go from homepage to users and back to homepage, homepage is not requested again. This only exists for one sessions and clears at a full page reload
 export default function Home() {
   return (
     <main>
