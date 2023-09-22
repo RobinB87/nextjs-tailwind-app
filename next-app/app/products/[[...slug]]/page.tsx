388ea44dcd0872b4498ab319d3e3906120f4639c
RobinB87 <robinbette@hotmail.com>
2023-09-22 08:45:26 +0000

    show query string params

--- next-app/app/products/[[...slug]]/page.tsx
+++ next-app/app/products/[[...slug]]/page.tsx
@@ -1,11 +1,17 @@
 interface Props {
   params: { slug: string[] };
+  searchParams: { sortOrder: string };
 }
 
 // use folder with [...slug] to be able to do something like /products/dairy/milk
 // wrap that in extra brackets [[...slug]] to make it optional and be able to do /products
-const ProductPage = ({ params: { slug } }: Props) => {
-  return <div>ProductPage {slug}</div>;
+// user searchParams to be able to use query string params, like: /products?sortOrder=name
+const ProductPage = ({ params: { slug }, searchParams: { sortOrder } }: Props) => {
+  return (
+    <div>
+      ProductPage {slug} {sortOrder}
+    </div>
+  );
 };
 
 export default ProductPage;
