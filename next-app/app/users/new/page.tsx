c0804428d248922e3ae0eb012e88eb25a5033ca2
RobinB87 <robinbette@hotmail.com>
2023-09-22 09:46:29 +0000

    show programmatic navigation

--- next-app/app/users/new/page.tsx
+++ next-app/app/users/new/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+// for programmatic navigation, be sure to import from next/navigation, NOT next/router
 const NewUserPage = () => {
-  return <div>NewUserPage</div>;
+  const router = useRouter();
+
+  return (
+    <button className="btn btn-primary" onClick={() => router.push("/users")}>
+      Create
+    </button>
+  );
 };
 
 export default NewUserPage;
--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import UsersTable from "./UsersTable";
 
 interface Props {
@@ -9,6 +11,9 @@ const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
   return (
     <>
       <h1>Users:</h1>
+      <Link href="/users/new" className="btn">
+        New User
+      </Link>
       {/* In Next.js, components can be rendered at build time (called Static Rendering) or at request time (called Dynamic Rendering). If we have pages or components with static data, we can pre-render them during build time to improve our application’s performance. 
       
       Rendering can happend static or dynamic. If no cache options are used in the fetch method, the below date will not change for a production build. 
