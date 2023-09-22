645a520444141402f3e1060ff01b131c6435a187
RobinB87 <robinbette@hotmail.com>
2023-09-22 08:31:30 +0000

    move table to own component

---
+++ next-app/app/users/UsersTable.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UsersTable = async () => {
+  // next js stores in a data cache based on a file system
+  // if the same url is fetched, next js gets it from the file system
+  // you have full control though, eg 'url', { cache: "no-store" }): always get new data
+  // or: 'url', { next: { revalidate: 10 } }) get new data every 10 seconds
+  // This is only implemented in the fetch function, NOT in for example axios!
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", { next: { revalidate: 100 } });
+  const users: User[] = await res.json();
+
+  return (
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((x) => (
+          <tr key={x.id}>
+            <td>{x.name}</td>
+            <td>{x.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UsersTable;
--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -1,18 +1,6 @@
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
+import UsersTable from "./UsersTable";
 
 const UsersPage = async () => {
-  // next js stores in a data cache based on a file system
-  // if the same url is fetched, next js gets it from the file system
-  // you have full control though, eg 'url', { cache: "no-store" }): always get new data
-  // or: 'url', { next: { revalidate: 10 } }) get new data every 10 seconds
-  // This is only implemented in the fetch function, NOT in for example axios!
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", { next: { revalidate: 100 } });
-  const users: User[] = await res.json();
-
   return (
     <>
       <h1>Users:</h1>
@@ -21,22 +9,7 @@ const UsersPage = async () => {
       Rendering can happend static or dynamic. If no cache options are used in the fetch method, the below date will not change for a production build. 
       When 'cache: "no-store"' is used, You will see a lambda in the build output, before the /users page, and an explanation that rendering happens at run time on the server side and not static */}
       {/* <p>{new Date().toLocaleTimeString()}</p> */}
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((x) => (
-            <tr key={x.id}>
-              <td>{x.name}</td>
-              <td>{x.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UsersTable />
     </>
   );
 };
