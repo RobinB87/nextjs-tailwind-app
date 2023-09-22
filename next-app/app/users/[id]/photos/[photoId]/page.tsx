5de5919de5755dec3937f9147c2ae85c14b5d9aa
RobinB87 <robinbette@hotmail.com>
2023-09-22 08:38:36 +0000

    show dynamic routes

---
+++ next-app/app/users/[id]/photos/[photoId]/page.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  params: { id: number; photoId: number };
+}
+
+const UserPhotosPage = ({ params: { id, photoId } }: Props) => {
+  return (
+    <div>
+      UserPhotosPage {id} {photoId}
+    </div>
+  );
+};
+
+export default UserPhotosPage;
