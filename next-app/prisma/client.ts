cfa45118a49b2507e02a0ea4542e04b894a71936
RobinB87 <robinbette@hotmail.com>
2023-09-24 11:16:30 +0000

    implement best practice for prisma client because of fast refresh in dev mode in nextjs

--- next-app/prisma/client.ts
+++ next-app/prisma/client.ts
@@ -1,5 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// Best practice for NextJS due to 'fast refresh'
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
