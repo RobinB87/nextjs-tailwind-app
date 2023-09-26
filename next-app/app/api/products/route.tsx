cee617d57bc4b0adcfa9360be90feb5444e1a0ab
RobinB87 <robinbette@hotmail.com>
2023-09-26 18:20:17 +0000

    update model add validation add get and post method

--- next-app/app/api/products/route.tsx
+++ next-app/app/api/products/route.tsx
@@ -1,16 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+import schema from "./schema";
 
-export const GET = (request: NextRequest) => {
-  return NextResponse.json([
-    {
-      id: 1,
-      name: "Milk",
-      price: 2.5,
+export const GET = async (request: NextRequest) => {
+  const products = await prisma.product.findMany();
+  return NextResponse.json(products);
+};
+
+export const POST = async (request: NextRequest) => {
+  const body = await request.json();
+  const validation = schema.safeParse(body);
+  if (!validation.success) return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const product = await prisma.product.findUnique({ where: { name: body.name } });
+  if (product) return NextResponse.json({ error: "product already exists" }, { status: 400 });
+
+  const newProduct = await prisma.product.create({
+    data: {
+      name: body.name,
+      price: body.price,
     },
-    {
-      id: 2,
-      name: "Bread",
-      price: 3.5,
-    },
-  ]);
+  });
+  return newProduct
+    ? NextResponse.json(newProduct)
+    : NextResponse.json({ error: "error while creating product" }, { status: 400 });
 };
