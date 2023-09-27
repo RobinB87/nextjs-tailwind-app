bc698b937774db91b11f3b8140545deff96574eb
RobinB87 <robinbette@hotmail.com>
2023-09-27 10:06:11 +0000

    example of how to use session in a server component

--- next-app/app/api/auth/[...nextauth]/route.ts
+++ next-app/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,16 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
+export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-});
+};
+
+const handler = NextAuth(authOptions);
 
 // export this function as a GET or a POST
 // any GET or POST request send to this endpoint will be handled inside this handle function
--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
+import { getServerSession } from "next-auth";
 
+import { authOptions } from "./api/auth/[...nextauth]/route";
 import ProductCard from "./components/ProductCard/ProductCard";
 
 // Link:
 //  - only downloads content of the target page (so if you click /users, it does not download the navbar etc). rsc in devtools stands for react_server_component
 //  - pre-fetches links that are in the viewport. If you run the app in production, the users page will be prefetched twice, once sorted by name and once sorted by email (as there are two Links). Improves performance
 //  - Caches pages on the client: if you go to a page you have seen before, nextjs does not do a request to backend. So if you go from homepage to users and back to homepage, homepage is not requested again. This only exists for one sessions and clears at a full page reload
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
-      <h1>Hello world</h1>
+      <h1>Hello {session && <span>{session.user?.name}</span>}</h1>
       {/* Next.js provides the Link component to enable client-side navigation. This means as the user navigates between pages, the new content is loaded quickly and smoothly without the entire page being reloaded.  */}
       <Link href="/users/">Users</Link>
       <ProductCard />
