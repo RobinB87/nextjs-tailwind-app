203e416573942af79887dbef00456836795d04cb
RobinB87 <robinbette@hotmail.com>
2023-09-27 10:19:05 +0000

    create middleware to protect routes

---
+++ next-app/middleware.ts
@@ -0,0 +1,19 @@
+export { default } from "next-auth/middleware";
+// import middleware from "next-auth/middleware";
+
+// create file with name middleware.ts / .js in root of project and a function called middleware
+// this gets called on every request
+// export const middleware = (request: NextRequest) => {
+//   return NextResponse.redirect(new URL("/new-page", request.url));
+// };
+
+// export default middleware;
+
+// execute only for certain paths by exporting this object, works then automagically
+export const config = {
+  // /users/:id*
+  // *: zero or more parameters
+  // +: one ore more
+  // ?: zero or more
+  matcher: ["/users/:id*"],
+};
