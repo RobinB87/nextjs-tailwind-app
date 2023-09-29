13becb1a1ab3673b1f3924d5f7943a15c84a3d13
RobinB87 <robinbette@hotmail.com>
2023-09-29 10:45:22 +0000

    delete global-error.tsx to fix build errors

--- next-app/app/global-error.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-// if you want to capture errors in the global layout.tsx file, you need this file.
-const GlobalError = () => {
-  return <div>GlobalError</div>;
-};
-
-export default GlobalError;
