8d063222dc4ca41a55e04b00363fb9488bf5d92a
RobinB87 <robinbette@hotmail.com>
2023-09-29 09:57:26 +0000

    fix build errors

--- next-app/app/api/users/[id]/route.tsx
+++ next-app/app/api/users/[id]/route.tsx
@@ -5,7 +5,7 @@ import prisma from "@/prisma/client";
 
 // can also declare a simple props object like this:
 export const GET = async (request: NextRequest, { params }: { params: { id: string } }) => {
-  const user = await prisma.user.findUnique({ where: { id: +params.id } });
+  const user = await prisma.user.findUnique({ where: { id: params.id } });
   return user ? NextResponse.json(user) : NextResponse.json({ error: "user not found" }, { status: 404 });
 };
 
@@ -14,7 +14,7 @@ export const PUT = async (request: NextRequest, { params }: { params: { id: stri
   const validation = schema.safeParse(body);
   if (!validation.success) return NextResponse.json(validation.error.errors, { status: 400 });
 
-  const user = await prisma.user.findUnique({ where: { id: +params.id } });
+  const user = await prisma.user.findUnique({ where: { id: params.id } });
   if (!user) return NextResponse.json({ error: "user not found" }, { status: 404 });
 
   const updatedUser = await prisma.user.update({
